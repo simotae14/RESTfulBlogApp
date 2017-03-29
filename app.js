3fca40559fa65ac4e3ac092dd3757ac903a4948b
Simone Taeggi <startae14@gmail.com>
2017-03-29 14:43:39 +0000

    Restful blog: New e Create

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.get("/", function(req, res) {
    res.redirect("/blogs"); 
 });
 
-// INDEX
+// INDEX ROUTE
 app.get("/blogs", function(req, res){
   // recupero tutti i blog
   Blog.find({}, function(err, blogs){
@@ -40,6 +40,25 @@ app.get("/blogs", function(req, res){
       console.log("ERROR!");
     } else {
       res.render("index", {blogs: blogs});
+    }
+  });
+});
+
+// NEW ROUTE
+app.get("/blogs/new", function(req, res) {
+   res.render("new"); 
+});
+
+// CREATE ROUTE
+app.post("/blogs", function(req,res){
+  // CREO IL BLOG
+  Blog.create(req.body.blog, function(err, newBlog){
+    if(err) {
+      // reindirizzo ancora alla view new
+      res.render("new");
+    } else {
+      // REDIRECT ALL'INDEX
+      res.redirect('/blogs');
     }
   });
 });
